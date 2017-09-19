da8ba0fe11d28f095849dd7c5cf4a266b26ae214
liu_chunzi <liu_chunzi@wuxiapptec.com>
2017-09-19 22:51:53 +0800

    add Reader Model

--- meiwen/login/models.py
+++ meiwen/login/models.py
@@ -13,8 +13,26 @@ from django_mysql.models import JSONField, Model
 from system_settings.common.model_utils import Char, Dtime, Bool, Text, ForeignKey, Dday, Integer, Float
 
 
-class reader(models.Model):
+class Reader(models.Model):
     """
     读者表，记录读者的信息
+    reader_id是每一个读者的唯一编号
+    vip_level为0时，代表读者不是vip
+    reader_id就是User表的id
     """
-    id = Char(max_length=255, null=True, db_index=True,)
+    reader_id = Char(max_length=255, db_index=True)
+    write_id = Char(max_length=255, default=null, null=True, blank=True)
+    nickname = Char(max_length=63, null=True, blank=True)
+    name = Char(max_length=63, null=True, blank=True)
+    gender = Char(max_length=63, null=True, blank=True)
+    experience = Integer(default=0, null=True, blank=True)
+    level = Integer(default=0, null=True, blank=True)
+    vip_level = Integer(default=0, null=True, blank=True)
+    account_balance = Float(default=0, null=True, blank=True)
+    birthday = Dday(null=True, blank=True)
+    additional_id = Integer(null=True, blank=True)
+    is_active = Bool(default=False, null=True, blank=True)
+    is_delete = Bool(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.reader_id
