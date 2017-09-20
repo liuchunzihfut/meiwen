20d0dea8d99ce1b32c3c44f90b079b8bf1a459ea
liu_chunzi <liu_chunzi@wuxiapptec.com>
2017-09-20 22:45:49 +0800

    add log in , no sign up

--- meiwen/login/views.py
+++ meiwen/login/views.py
@@ -2,7 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.contrib import auth
+import time
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the login index.")
+
+def userLogin(request):  
+    curtime=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime());  
+          
+    if request.method=='POST':  
+        print("POST")  
+        username=request.POST.get('name','')
+        password=request.POST.get('password','')
+        user= auth.authenticate(username=username,password=password)
+        if user and user.is_active:  
+            auth.login(request, user)
+            return HttpResponse("Hello, world. You're login success.")
+                
+    return HttpResponse("Hello, world. You're login failed.")
